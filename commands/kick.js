76f23a6ed6ff2ca8e9b1ea784ef7685d32483486
unknown <berksedatk@gmail.com>
2019-07-17 22:05:25 +0300

    Bug fixes

--- commands/kick.js
+++ commands/kick.js
@@ -21,7 +21,7 @@ module.exports = {
     if (!message.guild.member(member).kickable) return message.channel.send(":x: | You can't kick this user. They are too powerful.");
 
     try {
-      message.guild.kick(member, reason)
+      member.kick(reason)
     } catch (e) {
       message.channel.send("An error occured: " + e);
     }
