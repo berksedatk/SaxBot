627374d450713857e203c73a544b5d738792baec
unknown <berksedatk@gmail.com>
2019-07-20 17:53:52 +0300

    Add guildinfo.js

---
+++ commands/guildinfo.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const Discord = require('discord.js')
+const config = require('../config.json');
+const color = require('../data/colors.json');
+
+module.exports = {
+  name: 'guildinfo',
+  category: "Utility",
+  description: 'Lists the information about the guild you\'re in',
+  aliases: ['serverinfo'],
+  cooldown: 5,
+  guildOnly: 'true',
+  async execute(bot, message, args) {
+    const guild = message.guild;
+    let vccount = 0;
+    let textcount = 0;
+    guild.channels.map(c => {
+      if (c.type === "text") {
+        textcount += 1;
+      } else if (c.type === "vc") {
+        vccount += 1;
+      }
+    })
+
+    let guildEmbed = new Discord.RichEmbed()
+    .setTitle(guild.name)
+    .setThumbnail(guild.icon)
+    .addField("Guild Owner",`${guild.owner.username}#${guild.owner.discriminator}`)
+    .addField("Guild Create Date", guild.createdAt)
+    .addField("Member Count", guild.memberCount)
+    .addField("Channel Count", `Text: ${textcount}, VC: ${vccount}`)
+    .addField("Role Count", guild.roles.size)
+  }
+}
