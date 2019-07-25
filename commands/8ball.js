be227c3ce4c12191b853e402b09f94b40ab14147
unknown <berksedatk@gmail.com>
2019-07-25 17:05:30 +0300

    Bug fixes

--- commands/8ball.js
+++ commands/8ball.js
@@ -12,28 +12,30 @@ module.exports = {
     yes = ['Truth','Thats true','Yes, yes it is','Of course'];
     maybe = ['Hmm...','Maybe??','I dunno','I wish i knew that','Im not sure'];
     no = ['Nope.','Lies!','Uhh... No','Not really','I dont think thats true'];
-    const chance = Math.floor(Math.random() * 3);
-    const data = [];
+    const chance = Math.floor(Math.random() * 3)
 
-    data.push(ballEmbed = new Discord.RichEmbed())
-    data.push(ballEmbed.setTimestamp())
-    data.push(ballEmbed.setFooter("Requested by " + message.author.username, message.author.avatarURL))
-    data.push(ballEmbed.setTitle(args))
+    let ballEmbed = new Discord.RichEmbed()
+    .setTimestamp()
+    .setFooter("Requested by " + message.author.username, message.author.avatarURL)
+    .setTitle(args)
 
     if (chance === 0) {
-      data.push(ballEmbed.setColor("GREEN"))
-      ballAnswer = yes[Math.floor(Math.random() * yes.lenght)]
-      data.push(ballEmbed.addField("**Yes!**", ballAnswer))
+      answer = Math.floor(Math.random() * yes.lenght)
+      ballAnswer = yes[answer]
+      ballEmbed.addField("**Yes!**", ballAnswer)
+      ballEmbed.setColor("GREEN")
     } else if (chance === 1) {
-      data.push(ballEmbed.setColor("GOLD"))
-      ballAnswer = maybe[Math.floor(Math.random() * yes.lenght)]
-      data.push(ballEmbed.addField("**Maybe...**", ballAnswer))
+      answer = Math.floor(Math.random() * maybe.lenght)
+      ballAnswer = maybe[answer]
+      ballEmbed.addField("**Maybe...**", ballAnswer)
+      ballEmbed.setColor("GOLD")
     } else if (chance === 2) {
-      data.push(ballEmbed.setColor("RED"))
-      ballAnswer = no[Math.floor(Math.random() * yes.lenght)]
-      data.push(ballEmbed.addField("**No.**", ballAnswer))
+      answer = Math.floor(Math.random() * no.lenght)
+      ballAnswer = no[answer]
+      ballEmbed.addField("**No.**", ballAnswer)
+      ballEmbed.setColor("RED")
     }
-    data.push(message.channel.send(ballEmbed))
-    message.channel.send(data, {split: true})
+
+    message.channel.send(ballEmbed)
   }
 };
