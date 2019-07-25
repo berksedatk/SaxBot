e4fea8bac04db0c1de8c422c5ffde4c9b1547b43
unknown <berksedatk@gmail.com>
2019-07-25 21:57:44 +0300

    Add undefined.js

---
+++ commands/undefined.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'undefined',
+  category: "Fun",
+  description: 'undefined',
+  usage: 'undefined',
+  cooldown: 5,
+  async execute(bot, message, args) {
+    message.channel.send("undefined")
+  }
+}
